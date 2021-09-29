110d939d1016c70119dc27213c962e0c8f1c0aa0
4sdk <bobo19700@gmail.com>
2021-09-29 14:32:12 +0300

    up

--- modules/network_security_group/variables.tf
+++ modules/network_security_group/variables.tf
@@ -15,13 +15,13 @@ variable "name" {
 
 variable "security_rules" {
   type = list(object({
-    name                         = string
-    priority                     = number
-    direction                    = string
-    access                       = string
-    protocol                     = string
-    source_port_range           = string
-    destination_port_range      = string
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_port_range          = string
+    destination_port_range     = string
     source_address_prefix      = string
     destination_address_prefix = string
   }))
